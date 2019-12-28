ed2bd6518e4a42e9e248ede590065963c79311d8
FUTURETECH6 <48900010+FUTURETECH6@users.noreply.github.com>
2019-12-28 14:08:49 +0800

    UD

--- Display.v
+++ Display.v
@@ -34,7 +34,7 @@ module Display(
     parameter bird_HPos   = 320;
     parameter bird_Xwidth = 34;
     parameter bird_Ywidth = 24;
-    parameter slot_width  = 100;
+    parameter slot_width  = 60;
     parameter slot_height = 100;
     parameter land_height = 100;
     parameter SW_Bird     = 1;
@@ -58,8 +58,10 @@ module Display(
     /* Core Control Part */
     Bird_Ctrl BC_m0(.clk_ms(clkdiv[10]), .up_button(up_button), .state(state), .pip1_X(pip1_X), .pip1_Y(pip1_Y),
         .V_pos(bird_VPos), .isDead(isDead) );
-    Pipe_Generator PG_m0(.clk_2ms(clkdiv[11]), .state(state),
-        .pip_X(pip1_X), .pip_Y(pip1_Y), .score(score) );
+    //Pipe_Generator PG_m0(.clk_2ms(clkdiv[11]), .state(state),
+    //    .pip_X(pip1_X), .pip_Y(pip1_Y), .score(score) );
+	 assign pip1_X = 150;
+	 assign pip1_Y = 300;
     
     /* BackGround */
     wire [14:0] bg_addr;
@@ -80,20 +82,23 @@ module Display(
     wire [7:0] pip_addr_up, pip_addr_down;
     wire [11:0] BGR_upPip, BGR_downPip;
     wire [3:0] pU_abandon, pD_abandon;
+	 wire Opacity_up, Opacity_down;
 
-    assign inPipe = (Y_Addr > pip1_Y || Y_Addr < pip1_Y - slot_height) && X_Addr > pip1_X - slot_width && X_Addr < pip1_X;
-    assign pip_addr_up = (Y_Addr - pip1_Y) > 12 ? (pip1_X - X_Addr) : (Y_Addr - pip1_Y) * 20 + (pip1_X - X_Addr);
-    assign pip_addr_down = (pip1_Y - slot_height - Y_Addr) > 12 ? (pip1_X - X_Addr + 200) : (pip1_Y - slot_height - Y_Addr) * 20 + (pip1_X - X_Addr);
+    assign inPipe = (Y_Addr >= pip1_Y || Y_Addr <= pip1_Y - slot_height) && X_Addr > pip1_X - slot_width && X_Addr <= pip1_X;
+    assign pip_addr_down = (Y_Addr - pip1_Y) > 12 ? (pip1_X - X_Addr / 3) : (Y_Addr - pip1_Y) * 20 + (pip1_X - X_Addr / 3);
+    assign pip_addr_up = (pip1_Y - slot_height - Y_Addr) > 12 ? (pip1_X - X_Addr / 3 + 200) : (pip1_Y - slot_height - Y_Addr) * 20 + (pip1_X - X_Addr / 3);
 
-    pipe_up_20_12   PU_m0(.a(pip_addr_up),   .spo({pU_abandon, BGR_upPip}));
     pipe_down_20_12 PD_m0(.a(pip_addr_down), .spo({pD_abandon, BGR_downPip}));
+    pipe_up_20_12   PU_m0(.a(pip_addr_up),   .spo({pU_abandon, BGR_upPip}));
+	 assign Opacity_up = pU_abandon == 4'hF;
+	 assign Opacity_down = pD_abandon == 4'hF;
 
     /* Bird: Right_Down Side */
     wire [3:0] bird_opacity_00, bird_opacity_01, bird_opacity_02, bird_opacity_10, bird_opacity_11, bird_opacity_12;
     wire [11:0] bird_BGR_00, bird_BGR_01, bird_BGR_02, bird_BGR_10, bird_BGR_11, bird_BGR_12;
     wire [9:0] bird_addr;
     assign inBird = Y_Addr >= bird_VPos && Y_Addr < bird_VPos + bird_Ywidth && X_Addr <= bird_HPos && X_Addr > bird_HPos - bird_Xwidth;
-    assign bird_addr = inBird ? bird_Xwidth * (bird_VPos + bird_Ywidth - 1 - Y_Addr) + (bird_HPos - X_Addr) : 0;
+    assign bird_addr = inBird ? bird_Xwidth * (bird_VPos + bird_Ywidth - 1 - Y_Addr) + (X_Addr - (bird_HPos - bird_Xwidth + 1)) : 0;
     bird0_0 BG_B00_m0(.a(bird_addr), .spo({bird_opacity_00, bird_BGR_00}));
     bird0_1 BG_B01_m0(.a(bird_addr), .spo({bird_opacity_01, bird_BGR_01}));
     bird0_2 BG_B02_m0(.a(bird_addr), .spo({bird_opacity_02, bird_BGR_02}));
@@ -101,7 +106,7 @@ module Display(
     bird1_1 BG_B11_m0(.a(bird_addr), .spo({bird_opacity_11, bird_BGR_11}));
     bird1_2 BG_B12_m0(.a(bird_addr), .spo({bird_opacity_12, bird_BGR_12}));
     reg [1:0] clk_bird = 0;    // For wing shaking
-    always @(posedge clkdiv[24])
+    always @(posedge clkdiv[23])
         clk_bird = clk_bird == 3 ? 0 : clk_bird + 1;
     always @(clk_bird) begin
         case(clk_bird)
@@ -111,11 +116,11 @@ module Display(
             end
             1: begin
                 Opacity_Bird <= SW_OK[SW_Bird] ? bird_opacity_11 : bird_opacity_01;
-                BGR_Bird <= SW_OK[SW_Bird] ? bird_BGR_10 : bird_BGR_00;
+                BGR_Bird <= SW_OK[SW_Bird] ? bird_BGR_11 : bird_BGR_01;
             end
             2: begin
                 Opacity_Bird <= SW_OK[SW_Bird] ? bird_opacity_12 : bird_opacity_02;
-                BGR_Bird <= SW_OK[SW_Bird] ? bird_BGR_10 : bird_BGR_00;
+                BGR_Bird <= SW_OK[SW_Bird] ? bird_BGR_12 : bird_BGR_02;
             end
             3: begin
                 Opacity_Bird <= SW_OK[SW_Bird] ? bird_opacity_11 : bird_opacity_01;
@@ -133,11 +138,11 @@ module Display(
             {BGR_B,BGR_G,BGR_R} <= BGR_Bird;
         else if(inLand)
             {BGR_B,BGR_G,BGR_R} <= BGR_Land;
-        else if(inPipe) begin
-            if(Y_Addr > pip1_Y)
-                {BGR_B,BGR_G,BGR_R} <= BGR_upPip;
-            if(Y_Addr < pip1_Y - slot_height)
+        else if(inPipe && (Opacity_up || Opacity_down)) begin
+            if(Y_Addr > pip1_Y && Opacity_down)
                 {BGR_B,BGR_G,BGR_R} <= BGR_downPip;
+            if(Y_Addr < pip1_Y - slot_height && Opacity_up)
+                {BGR_B,BGR_G,BGR_R} <= BGR_upPip;
         end
         else
             {BGR_B,BGR_G,BGR_R} <= SW_OK[SW_BG] ? BGR_Night : BGR_Day;
